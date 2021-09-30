b4e7291543c9edb00cbe2a9459233a2e996a202d
SeungYn <kwls0407@gmail.com>
2021-10-01 01:08:27 +0900

    무지 등록 버튼 수정

--- .history/src/components/muziForm/muziForm_20211001005936.jsx
+++ .history/src/components/muziForm/muziForm_20211001005936.jsx
@@ -1,39 +1,42 @@
 import React, { Component } from 'react';
-import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
-import styles from './detailMuziCommentForm.module.css';
+import styles from './muziForm.module.css';
 
-class DetailMuziCommentForm extends Component {
+class MuziForm extends Component {
   textRef = React.createRef();
   formRef = React.createRef();
-
   textResize = () => {
     const obj = this.textRef.current;
     obj.style.height = 'auto';
     obj.style.height = obj.scrollHeight + 'px';
   };
-  //스크롤 자동 나락
-  // scrollAuto = (jsxRef) => {
-  //   jsxRef.current.scrollIntoView({
-  //     behavior: 'smooth',
-  //     block: 'end',
-  //   });
-  // };
+
   onSubmit = (event) => {
     event.preventDefault();
-    const comment = this.textRef.current.value;
-    comment && this.props.onAddMuziComment(this.props.selectedMuzi.id, comment);
+    const text = this.textRef.current.value;
+    text && this.props.onAddMuzi(text, this.props.selectedMuzi);
     this.formRef.current.reset();
-
     //텍스트를 입력호 등록 후 크기 축소
     const obj = this.textRef.current;
     this.textRef.current.value = '';
     obj.style.height = 'auto';
   };
 
+  //\n을 </br>로 치환해서 화면에 나올수있게하는 메서드
+  nl2br = (text) => {
+    const textAll = text.split('\n');
+    const filteredText = textAll.map((text, i) => {
+      if (textAll.length - 1 === i) {
+        return text;
+      }
+      return text + '<br />';
+    });
+    return filteredText.join('');
+  };
+
   render() {
     return (
       <form
-        action='#'
+        action=''
         ref={this.formRef}
         className={styles.form}
         onSubmit={this.onSubmit}
@@ -46,8 +49,8 @@ class DetailMuziCommentForm extends Component {
             onKeyDown={this.textResize}
           ></textarea>
           <div className={styles.bottom}>
-            <button className={styles.commentButton} onClick={this.asd}>
-              <i className='far fa-paper-plane'></i>
+            <button className={styles.muziButton}>
+              <i class='far fa-paper-plane'></i>
             </button>
           </div>
         </div>
@@ -56,4 +59,4 @@ class DetailMuziCommentForm extends Component {
   }
 }
 
-export default DetailMuziCommentForm;
+export default MuziForm;
